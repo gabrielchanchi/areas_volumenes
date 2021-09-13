e091e771b351f1071403143be77fbcc7e56d2cd7
gabrielchanchi <56714173+gabrielchanchi@users.noreply.github.com>
2021-09-13 14:16:29 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -11,12 +11,12 @@ def calcular_volumen_esfera(radio)
 	area=(4/3)*math.pi*radio**3
 	return volumen
 	 
-def calular_area_rombo (Diagonal,diagonal1 ):
-  area=((Diagonal*diagonal1)/2)
-  return area
+def calular_area_rombo (diagonal,diagonal1 ):
+ 	area=((diagonal*diagonal1)/2)
+ 	return area
 
 def calcular_volumen_cubo(lado):
-  volumen=lado**3
+ 	volumen=lado**3
 	return volumen
 
 def calcular_area_esfera (radio):
