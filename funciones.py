db63d60888969a5b834b8b8b7fdb760002d79ae3
Deimer99 <68717236+Deimer99@users.noreply.github.com>
2021-09-13 14:03:26 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,3 +1,4 @@
+import math
 def calcular_area_cuadrado (lado):
 	area=lado*lado
 	return area
@@ -6,10 +7,11 @@ def calcular_volumen_paralelepipedo(base,altura, alto):
 	volumen=base*altura*alto
 	return volumen
 
-def calcular_volumen_cubo(lado):
-	volumen=lado**3
-	return volumen
-	
+#print(f"El volumen de un cilindro es {calcular_volumen_cilindro(4)}")
+def calcular_volumen_cilindro(radio,altura):
+	volumenc=math.pi*radio**2*altura
+	return volumenc
+		
 #print(f"El area del cuadrado es {calcular_area_cuadrado(4)}")
 
 	 
